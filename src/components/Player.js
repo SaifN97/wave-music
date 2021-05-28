1e643c5eba8730ed7e13dad69b0074919bcc8602
SaifN97 <saifnarpali@gmail.com>
2021-05-28 12:44:22 +0530

    Initial setup with play/pause

---
+++ src/components/Player.js
@@ -0,0 +1,48 @@
+import React, { useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+  // Ref
+  const audioRef = useRef(null);
+  const playSongHandler = () => {
+    // Event Handler
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(!isPlaying);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(!isPlaying);
+    }
+  };
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>start time</p>
+        <input type="range" />
+        <p>end time</p>
+      </div>
+      <div className="play-control">
+        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          className="play"
+          size="2x"
+          icon={faPlay}
+        />
+        <FontAwesomeIcon
+          className="skip-forward"
+          size="2x"
+          icon={faAngleRight}
+        />
+        <audio ref={audioRef} src={currentSong.audio}></audio>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
